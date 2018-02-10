b39287d9a3feef64ee46b1964732e67a43313cca
rotten-hide <hidetake6991@gmail.com>
2018-02-10 12:37:23 +0900

    fix: login methon enable

--- app/urls.py
+++ app/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
-    # url(r'login', views.login, name='login'),
+    url(r'login', views.login, name='login'),
     url(r'', views.index, name='index'),
 ]
