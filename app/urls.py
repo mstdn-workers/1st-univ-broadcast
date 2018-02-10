4825d166a02176b495c57e821a64501786d0fe9a
rotten-hide <hidetake6991@gmail.com>
2018-02-10 17:53:14 +0900

    mod: loginページレイアウト

--- app/urls.py
+++ app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     url(r'^login$', views.login, name='login'),
     url(r'^redirected$', views.redirected, name='redirected'),
+    url(r'^redirect2auth$', views.redirect2auth, name='redirect2auth'),
     url(r'^$', views.index, name='index'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
 from django.http import HttpResponse
+from mastodon import Mastodon
+import os
+
+MASTODON_BASE_URL = os.environ['MASTODON_BASE_URL']
+CLIENT_ID = os.environ['CLIENT_ID'] 
+CLIENT_SECRET = os.environ['CLIENT_SECRET']
+REDIRECT_URI = os.environ['REDIRECT_URI']
+
 
 # Create your views here.
 def index(request):
@@ -13,6 +21,9 @@ def index(request):
 
 def login(request):
     return render(request, 'app/login.html')
+
+def redirect2auth(request):
+    return HttpResponse("リダイレクト中です...")
 
 def redirected(request):
     return HttpResponse(str(request.POST))
