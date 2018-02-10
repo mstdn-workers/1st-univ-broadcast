6f6f8daca7ab92fcebb3f288a364ba4ffb2ec163
rotten-hide <hidetake6991@gmail.com>
2018-02-10 18:30:46 +0900

    fix: namespace

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url, include
 from . import views
 
+app_name = 'app'
 urlpatterns = [
     url(r'^login$', views.login, name='login'),
     url(r'^redirected$', views.redirected, name='redirected'),
