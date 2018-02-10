c2e18d85ae2000a14e47b91d586b44369114bafe
rotten-hide <hidetake6991@gmail.com>
2018-02-10 18:21:48 +0900

    fix: redirect

--- app/views.py
+++ app/views.py
@@ -7,16 +7,17 @@ import os
 MASTODON_BASE_URL = os.environ['MASTODON_BASE_URL']
 CLIENT_ID = os.environ['CLIENT_ID'] 
 CLIENT_SECRET = os.environ['CLIENT_SECRET']
+ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
 REDIRECT_URI = os.environ['REDIRECT_URI']
 
 
 # Create your views here.
 def index(request):
-    access_token = request.COOKIES.get('access_token')
+    access_token = request.COOKIES.get('key')
     print(access_token)
     if access_token == None:
         print("true")
-        return redirect("/login")    
+        return redirect("app:login")    
     return render(request, 'app/index.html')
 
 def login(request):
